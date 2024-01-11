b188e8bed1d5acdd77cac81cf8cf3a474a42b5ca
DarekP <darek.piechota@gmail.com>
2024-01-11 18:31:10 +0100

    Angular Basics end

--- Schwarzmuller/my-first-app/src/app/servers/servers.component.ts
+++ Schwarzmuller/my-first-app/src/app/servers/servers.component.ts
@@ -5,11 +5,34 @@ import { Component, OnInit } from '@angular/core';
   // selector: '.app-servers',
   selector: 'app-servers',
   // templateUrl: './servers.component.html',
-  template: ` <app-server></app-server><app-server></app-server>`,
+  // template: ` <app-server></app-server><app-server></app-server>`,
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+  serverCreated = false;
+  servers = ['TestServer', 'TestServer 2'];
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {}
+
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      'Server was created! Name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event: Event) {
+    // console.log(event);  (<Html...>.value  to jest rzutowanie typu skad pochodzi value - wczesniej nie wiedział)
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }

eac4f3e9550ae403df93682c26ff15b099bb6dcc
DarekP <darek.piechota@gmail.com>
2024-01-22 13:47:19 +0100

    #ShopingApp adding shoping list edit section

---
+++ Schwarzmuller/FirstShoppingApp/src/app/shopping-list/shared/ingredient.model.ts
@@ -0,0 +1,11 @@
+export class Ingredient {
+  // public name: string;
+  // public amount: number;
+
+  // constructor(name: string, amount: number) {
+  //   this.name = name;
+  //   this.amount = amount;
+  // }
+  //To samo co wyzej!
+  constructor(public name: string, public amount: number) {}
+}

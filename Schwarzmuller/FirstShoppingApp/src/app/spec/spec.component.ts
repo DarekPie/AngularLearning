65cbc0b5d4217464ee1f5ef7fcc5c5c8115a74f7
DarekP <darek.piechota@gmail.com>
2024-01-22 09:11:54 +0100

    #ShopingApp adding struture of components

---
+++ Schwarzmuller/FirstShoppingApp/src/app/spec/spec.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spec',
+  templateUrl: './spec.component.html',
+  styleUrl: './spec.component.css'
+})
+export class SpecComponent {
+
+}

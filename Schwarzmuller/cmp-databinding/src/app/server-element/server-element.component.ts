31e9b0f84e2d5a0a46effd3e3b0299ffbae41b4e
DarekP <darek.piechota@gmail.com>
2024-01-22 14:51:18 +0100

    #Components and databinding Spliting apps into components

---
+++ Schwarzmuller/cmp-databinding/src/app/server-element/server-element.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server-element',
+  templateUrl: './server-element.component.html',
+  styleUrl: './server-element.component.css',
+})
+export class ServerElementComponent implements OnInit {
+  constructor() {}
+  ngOnInit(): void {}
+}

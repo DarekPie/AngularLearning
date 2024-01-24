7ed37949a99f163b70b70341f4b1921d1d36956e
DarekP <darek.piechota@gmail.com>
2024-01-24 18:50:17 +0100

    #Components and databinding binding custom properties

--- Schwarzmuller/cmp-databinding/src/app/app.component.ts
+++ Schwarzmuller/cmp-databinding/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  serverElements = [];
+  serverElements = [
+    { type: 'server', name: 'Testserver', content: 'Just a test!' },
+  ];
   newServerName = '';
   newServerContent = '';
-
 }

e9b72a98db58dce57042430138a737f6a531defc
DarekP <darek.piechota@gmail.com>
2024-02-10 12:11:19 +0100

    #ShoppingApp Ading desc names to recipes and adding shopping list

--- Schwarzmuller/FirstShoppingApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ Schwarzmuller/FirstShoppingApp/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  EventEmitter,
+  Output,
+} from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
-  styleUrl: './shopping-edit.component.css'
+  styleUrl: './shopping-edit.component.css',
 })
-export class ShoppingEditComponent {
+export class ShoppingEditComponent implements OnInit {
+  @ViewChild('...', { static: true }) nameInputRef: ElementRef;
+  @ViewChild('...', { static: true }) amountInputRef: ElementRef;
+  // ingredientAdded = new EventEmitter<{name: string, amount: number}>();
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  onAddItem() {
+    const ingName = this.nameInputRef.nativeElement;
+    const ingAmount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(ingName, ingAmount);
+    this.ingredientAdded.emit(newIngredient);
+  }
 }
